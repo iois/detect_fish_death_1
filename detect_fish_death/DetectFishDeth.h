e31d9427f0cf68f534179f96839f1e81e356d604
liyi <lrwlisense@163.com>
2015-06-27 21:38:24 +0800

    201506272138

--- detect_fish_death/DetectFishDeth.h
+++ detect_fish_death/DetectFishDeth.h
@@ -1,42 +1,143 @@
 #include <vector>
 #include "opencv2/opencv.hpp"
+#include <list>
+#include <math.h>
+#include <stdlib.h> 
 
 using namespace std;
 using namespace cv;
+
+const int SIZE_FRAME = 15;
+const double MAX_SPEED = 50;
+const double MAX_AREA = 1000;
+const double MAX_POZ = 600;
 class DetectFishDeth
 {
 	//   检测鱼是否死亡，概率判断
 public:
-	DetectFishDeth(const vector<vector<Point> > &contours);
+	DetectFishDeth(const vector<vector<Point> > &contours, int border_position, int avg_area);
 	~DetectFishDeth();
-	//Get_relativepoz();
+	vector<double> get_relativepoz()
+	{
+		vector<double> relative_poz;
+		for (int i = 0; i < num_fish; ++i)
+		{
+			relative_poz.push_back(abs(border_position - center_point[i].x) / MAX_POZ);
+		}
+		return relative_poz;
+	};    //得到鱼到边界的距离
+	vector<double> get_speed();
+	vector<double> get_diff_area();
+
+
+	vector<double> get_prob_death(const vector<vector<Point> > &contours);
 
 
 private:
+
+	void  update_data(const vector<vector<Point> > &contours);//
+
+
 	vector <Point>  center_point;
-	vector <double>  speed;
-	vector <int>  contour_area;
+	vector <double>  contour_area;
 	int num_fish;
+	vector<vector<Point> > trajectory;
 
-
+	double border_position;
+	double avg_area;
 };
 
-DetectFishDeth::DetectFishDeth(const vector<vector<Point> > &contours)
+DetectFishDeth::DetectFishDeth(const vector<vector<Point> > &contours, int bp, int _avg_area)
 {
 	num_fish = contours.size();
-	
+
+	border_position = bp;
+	avg_area = _avg_area;
+
 	for (int i = 0; i < num_fish; ++i)
 	{
-		//计算鱼的中心点
-		Moments m=moments(contours[i]);
-		center_point.push_back(Point(m.m10 / m.m00, m.m01 / m.m00));
+		vector <Point> tra(SIZE_FRAME, { 0, 0 });
 
-		// 速度初始化为0；
-		speed.push_back(0);
-		
-		//调用外部函数求轮廓面积
-		contour_area.push_back(contourArea(contours[i]));
+		trajectory.push_back(tra);
+
+		Moments m = moments(contours[i]);
+
+		center_point.push_back(Point(m.m10 / m.m00, m.m01 / m.m00));//计算鱼的中心点
+
+		contour_area.push_back(contourArea(contours[i]));//调用外部函数求轮廓面积
 	}
+}
+void DetectFishDeth::update_data(const vector<vector<Point> > &contours)
+{
+	for (int i = 0; i < num_fish; ++i)
+	{
+		Moments m = moments(contours[i]);
+		Point center = Point(m.m10 / m.m00, m.m01 / m.m00);
+		trajectory[i].insert(trajectory[i].cbegin(), center);
+		trajectory[i].pop_back();
+		center_point[i] = center;
+		contour_area[i] = contourArea(contours[i]);
+	}
+}
+vector<double> DetectFishDeth::get_prob_death(const vector<vector<Point> > &contours)
+{
+	this->update_data(contours);
+
+	vector<double> prob;
+	vector<double> x = get_relativepoz();   //位置	
+	vector<double> diffarea = get_diff_area();   //面积
+	vector<double> _speed = get_speed();   //速度
+
+	vector<double> y1;
+	vector<double> y2;
+	vector<double> y3;
+
+	for (int i = 0; i < num_fish; ++i)
+	{
+		y3.push_back(-log(_speed[i]));
+		y2.push_back(-log(1 - diffarea[i]));
+		y1.push_back(-log(x[i]));
+	}
+	double theta1 = 1, theta2 = 1, theta3 = 1;
+	for (int i = 0; i < num_fish; ++i)
+	{
+		prob.push_back(tanh(theta1*y1[i] + theta2*y2[i] + theta3*y3[i]));
+	}
+	return prob;
+}
+vector<double> DetectFishDeth::get_speed()
+{
+	vector<double> speed;
+
+
+	for (int i = 0; i < num_fish; ++i)
+	{
+		double x_sum = 0;
+		double y_sum = 0;
+		for (int j = 1; j < trajectory[i].size(); ++j)
+		{
+			x_sum += abs(trajectory[i][j].x - trajectory[i][j - 1].x);
+			y_sum += abs(trajectory[i][j].y - trajectory[i][j - 1].y);
+		}
+		speed.push_back(sqrt(x_sum*x_sum + y_sum*y_sum) / MAX_SPEED);
+	}
+	return speed;
+}
+
+vector<double> DetectFishDeth::get_diff_area()
+{
+	//todo: 考虑重叠的情况
+	vector<double> diff_area;
+	for (int i = 0; i < num_fish; ++i)
+	{
+		if (contour_area[i] > avg_area){
+			diff_area.push_back((contour_area[i] - avg_area) / MAX_AREA);
+		}
+		else{
+			diff_area.push_back(0.05);
+		}
+	}
+	return diff_area;
 }
 
 DetectFishDeth::~DetectFishDeth()
--- detect_fish_death/Source.cpp
+++ detect_fish_death/Source.cpp
@@ -1,5 +1,8 @@
+/*背景差分*/
+
 #include <iostream>
 #include "opencv2/opencv.hpp"
+#include"DetectFishDeth.h"
 
 using namespace std;
 using namespace cv;
@@ -11,24 +14,25 @@ Scalar color = Scalar(255, 255, 0);
 int main()
 {
 	Mat background;
-	background = imread("background.bmp",0);//0:gray
+	background = imread("background.bmp", 0);//0:gray
 
-	VideoCapture vidCapture("Video179.wmv");
+	VideoCapture vidCapture("20140109.avi");
 	Mat frame;
 
 	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
 
+	DetectFishDeth *detect_fish_deth = nullptr;
 	for (;;){
 		vidCapture >> frame;
 
 		cvtColor(frame, src_gray, COLOR_RGB2GRAY);
 
-		absdiff(src_gray, background, dst);//背景差分
-		
-		bitwise_xor(Scalar(255, 0, 0, 0),dst, dst);//xor,颜色取反
+		absdiff(src_gray, background, dst);//差分
+
+		bitwise_xor(Scalar(255, 0, 0, 0), dst, dst);//xor,颜色取反
 
 		GaussianBlur(dst, dst, Size(5, 5), 0, 0);
-		
+
 		threshold(dst, dst, 230, 255, 0);//阈值分割
 
 		imshow("camera 1 frame", dst);
@@ -37,27 +41,56 @@ int main()
 		vector<Vec4i> hierarchy;
 
 		findContours(dst, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-		
-		//
-		//根据输入鱼的轮廓，判断与是否死亡
-		//
 
+		vector<vector<Point> > another_contours;
+		for (int i = 0; i < 3; ++i){
+			another_contours.push_back(contours[i + 1]);
+		}
+		if (!detect_fish_deth){
+			detect_fish_deth = new DetectFishDeth(another_contours, 640, 600);
+		}
+		else{
+			vector<double> prob = detect_fish_deth->get_prob_death(another_contours);
+
+			vector<double> speed = detect_fish_deth->get_speed();
+			vector<double> area = detect_fish_deth->get_diff_area();
+			vector<double> poz = detect_fish_deth->get_relativepoz();
+			for each (double var in speed)
+			{
+				cout << "speed= " << var << " ";
+			}cout << endl;
+			for each (double var in area)
+			{
+				cout << "area= " << var << " ";
+			}cout << endl;
+			for each (double var in poz)
+			{
+				cout << "poz= " << var << " ";
+			}
+			cout << endl;
+			for each (double var in prob)
+			{
+				cout << "prob= " << var << " ";
+			}cout << endl;
+		}
 		/// Draw contours
 		Mat drawing = Mat::zeros(dst.size(), CV_8UC3);
 		for (int i = 0; i< contours.size(); i++)
 		{
-			if (contours[i].size() >4){
+			//if (contours[i].size() >4)
+			{
 				drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-
+				/*
 				for (int j = 0; j < contours[i].size(); ++j){
-					cout << contours[i][j].x << " " << contours[i][j].y << endl;
+				cout << contours[i][j].x << " " << contours[i][j].y << endl;
 				}
+				*/
 			}
 		}
 		imshow("Contours", drawing);
-		
+
 		int key = waitKey(66);
-		if (key == 27){break;}
+		if (key == 27){ break; }
 	}
 	return 0;
 }
